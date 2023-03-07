a4486b7aa50c9b05039b6ced3cefb2c6fe6b2f60
krai54rus <krai54rus@gmail.com>
2023-03-07 15:17:23 +0300

    test add files

--- src/components/test/TestWrapper.vue
+++ src/components/test/TestWrapper.vue
@@ -7,6 +7,7 @@
   <div class="n-wp-100 n-p-16" :class="$style['main-wrapper']">
     <WidgetBlock title="ToDo" :items="data"></WidgetBlock>
     <WidgetBlock title="ToDo" :items="data"></WidgetBlock>
+    <WidgetBlock title="ToDo" :items="data"></WidgetBlock>
   </div>
 </template>
 
